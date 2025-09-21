4e162fe55ffba8710db3c1b801b4065139dfcc9b
Eduards Oss <n00230867@iadt.ie>
2025-09-22 00:13:19 +0100

    first commit

---
+++ routes/api.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\ProductController;
+
+Route::controller(AuthController::class)->group(function(){
+    Route::post('register', 'register');
+    Route::post('login', 'login');
+});
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::middleware('auth:sanctum')->group( function () {
+    Route::apiResource('products', ProductController::class);
+});
